252f4d8f23bb6e3a019d575a9063bce7770f472d
Leon <118800175+0n3W4y7ick3t@users.noreply.github.com>
2024-03-31 17:12:56 +0900

    Update rules.mk

--- keyboards/crkbd/keymaps/via_v4_miryoku/rules.mk
+++ keyboards/crkbd/keymaps/via_v4_miryoku/rules.mk
@@ -1,5 +1,7 @@
-MOUSEKEY_ENABLE = no     # Mouse keys
-VIA_ENABLE      = yes    # Enable VIA
+MOUSEKEY_ENABLE = no       # Mouse keys
+VIA_ENABLE      = false    # disable VIA
 LTO_ENABLE      = yes
 CAPS_WORD_ENABLE = yes
-ENCODER_MAP_ENABLE = no # I dont use encoder
+DYNAMIC_MACRO_ENABLE = yes
+ENCODER_MAP_ENABLE = no    # I dont use encoder
+BOOTLOADER = rp2040
